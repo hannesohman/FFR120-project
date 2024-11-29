260c914193aae5f6e01a8e98d9452eaca0ad0eda
Edvin Ahlström <edvin@edvinlaptop>
2024-11-29 11:48:31 +0100

    fortsätter på vektorisering

--- diffuse_spread_recover_vectorized.py
+++ diffuse_spread_recover_vectorized.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-
 # an attempt to redo the dsr algorithm faster
-#
+
+
 def diffuse_spread_recover(x, y, status, d, beta, gamma, L, alpha):
     """
     Function performing the diffusion step, the infection step, and the
@@ -31,6 +31,7 @@ def diffuse_spread_recover(x, y, status, d, beta, gamma, L, alpha):
     y = np.where(np.logical_and(agent_diffuses, y_diffuses), y + move_direction, y)
 
     # Enforce pbc.
+    # TODO change to reflective boundaries.
     x = x % L
     y = y % L
 
@@ -41,15 +42,11 @@ def diffuse_spread_recover(x, y, status, d, beta, gamma, L, alpha):
     suseptible_again = np.random.rand(N) < alpha
     status = np.where(np.logical_and(status == 2, suseptible_again), 0, status)
 
-    # recovered = np.where(status == 2)[0]
-    # for r in recovered:
-    #     # Check whether the recovered becomes suseptible again.
-    #     if np.random.rand() < alpha:
-    #         status[r] = 0
+    # vectorized version
+    x_infected = x[status == 1]
+    y_infected = y[status == 1]
 
-    # Spreading disease step.
     infected = np.where(status == 1)[0]
-
     for i in infected:
         # Check whether other particles share the same position.
         same_x = np.where(x == x[i])
