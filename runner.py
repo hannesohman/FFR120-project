707251e7b21180323c918af05ed08c79df1d4d62
Edvin Ahlström <edvin@edvinlaptop>
2024-12-09 09:40:31 +0100

    bug fix + moved old parameters

--- runner.py
+++ runner.py
@@ -7,22 +7,6 @@ from main import run_simulation
 import numpy as np
 import matplotlib.pyplot as plt
 
-N_indiv = 2000  # * Antalet individer som ska simuleras
-simulation_days = 30
-dt = 0.1  # time step (measured in days)
-I0 = 2  # Start value of I
-
-# SIR params = beta, gamma, theta, alpha
-beta = 1 / 1.8
-gamma = 1 / 14
-theta = 0.0001
-alpha = 1 / 25
-sus_mean = 1
-sus_std = 0.2
-vaccine_mode = "risk group"
-vaccine_factor = 0.20  # factor by which the vaccination decreases suseptability
-vaccine_factor = 1.0  # factor by which the vaccination decreases suseptability
-fraction_weakest = 0.5
 
 parameters = [
     1 / 1.8,
@@ -30,14 +14,13 @@ parameters = [
     0.0001,
     1 / 25,
     2000,
-    30,
+    300,
     0.1,
     2,
     1,
     0.2,
     "risk group",
     0.2,
-    1.0,
     0.5,
 ]
 S, I, R, D = run_simulation(*parameters)
@@ -55,3 +38,25 @@ plt.xlabel("time")
 plt.ylabel("S, I, R, D")
 
 plt.show()
+
+
+"""
+# "original" parameters
+N_indiv = 2000  # * Antalet individer som ska simuleras
+simulation_days = 300
+dt = 0.1  # time step (measured in days)
+I0 = 2  # Start value of I
+
+# SIR params = beta, gamma, theta, alpha
+beta = 1 / 1.8
+gamma = 1 / 14
+theta = 0.0001
+alpha = 1 / 25
+sus_mean = 1
+sus_std = 0.2
+vaccine_mode = "risk group"
+vaccine_factor = 0.20  # factor by which the vaccination decreases suseptability
+vaccine_factor = 1.0  # factor by which the vaccination decreases suseptability
+fraction_weakest = 0.5
+
+"""
