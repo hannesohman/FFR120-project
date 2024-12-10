6f3cfaad81eed94f6af4ec20fedd4c8ed6a35be8
Edvin Ahlström <edvin@edvinlaptop>
2024-12-10 19:27:14 +0100

    changed parameters

--- runner.py
+++ runner.py
@@ -78,7 +78,7 @@ repeats = 4
 
 # loop over multiple vaccination times, no lockdown
 
-vaccination_times = [0.03, 0.1, 1]
+vaccination_times = [0.15, 0.2, 0.25, 0.3]
 for vaccination_time in vaccination_times:
     for i in range(repeats):
         parameters["vaccine_time"] = vaccination_time
@@ -91,7 +91,7 @@ for vaccination_time in vaccination_times:
 
 # loop over multiple lockdown times (fixed vaccination time at )
 
-lockdown_times = [0.03, 0.1]
+lockdown_times = vaccination_times
 lockdown_times = np.linspace(0, max_infection_rate, num=runs)
 lockdown_times = np.round(lockdown_times, decimals=2)  # round for cleanliness
 for lockdown_time in lockdown_times:
